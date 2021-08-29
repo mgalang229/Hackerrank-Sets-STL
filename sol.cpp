58f66ff25cbfa1518b0351990540e4a11aebffc2
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-08-29 17:40:47 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-#define ll long long
+using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
 	int q;
 	cin >> q;
 	set<int> s;
-	for(int i=0; i<q; ++i) {
+	// simply follow the instructions in the problem statement
+	for (int i = 0; i < q; i++) {
 		int y, x;
 		cin >> y >> x;
-		if (y==1)
+		if (y == 1) {
 			s.insert(x);
-		else if(y==2)
+		} else if (y == 2) {
 			s.erase(x);
-		else
-			cout << (s.find(x)!=s.end()?"Yes":"No") << "\n";
+		} else {
+			cout << (s.find(x) != s.end() ? "Yes" : "No") << '\n';
+		}
 	}
+	return 0;
 }
